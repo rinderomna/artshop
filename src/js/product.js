371c2b2a2589a5c9dc820e1f57da32b6fd983fa4
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2023-04-25 16:21:05 -0300

    controle de produto

---
+++ src/js/product.js
@@ -0,0 +1,22 @@
+const controlAdjusters = document.querySelectorAll("[data-control]");
+
+controlAdjusters.forEach(controlAdjuster => {
+    controlAdjuster.addEventListener("click", (occured_event) => {
+        manipulateValue(occured_event.target.parentNode, occured_event.target.dataset.control);
+    });
+});
+
+function manipulateValue(control, operation) {
+    const counter = control.querySelector("[data-display]");
+
+    let value = parseInt(counter.innerText);
+
+    if (value === 1 && operation === "less") return;
+    if (value === 999 && operation === "plus") return;
+
+    if (operation === "plus") {
+        counter.innerText = String(value + 1);
+    } else if (operation == "less") {
+        counter.innerText = String(value - 1);
+    }
+}
