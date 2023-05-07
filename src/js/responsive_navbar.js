cafba3e4c0a9d86dbb4a9fa9382446c520c6d21e
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2023-05-07 12:49:01 -0300

    ajeitando menu

--- src/js/responsive_navbar.js
+++ src/js/responsive_navbar.js
@@ -1,15 +1,12 @@
 // Menu responsivo
-
 "use strict";
 
 function toggleResponsiveMenu() {
     let x = document.getElementById("homeNavBar");
-    
+
     if (x.className === "homeNavBar") { // Habilitar menu
         x.className += " responsive";
-    } 
-    
-    else { // Desabilitar menu
+    } else { // Desabilitar menu
         x.className = "homeNavBar";
     }
-  } 
+}
