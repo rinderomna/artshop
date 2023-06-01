492fd396895aa77fa76204c92fbc6caefc25a561
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2023-06-01 10:58:25 -0300

    começando a modularizar

---
+++ react_app/src/components/LogoLink/LogoLink.js
@@ -0,0 +1,16 @@
+import './LogoLink.css'
+
+function LogoLink() {
+  return (
+    <div  className="logo">
+      <a href="#">
+        <img
+          src="../../assets/visual_identity/logo/jagua_logo.png"
+          alt="Logo do site"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default LogoLink;
--- react_app/src/pages/App.js
+++ react_app/src/pages/App.js
@@ -1,8 +1,8 @@
+import Navbar from "../components/Navbar/Navbar.js";
+
 function App() {
   return (
-    <div className="App">
-        Hello World
-    </div>
+    <Navbar />
   );
 }
 
