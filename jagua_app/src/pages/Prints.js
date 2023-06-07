d0bb83f2acde32f713679ae992f868e48b66ffcf
laura <laura.scotelari@usp.br>
2023-06-07 12:48:58 -0300

    Consertndo sizes na page de stickers, shirts, prints

--- jagua_app/src/pages/Prints.js
+++ jagua_app/src/pages/Prints.js
@@ -10,41 +10,62 @@ import printEspelho from "../assets/visual_identity/prints/print_celular.png";
 import printAbducao from "../assets/visual_identity/prints/print_irmas.png";
 
 const Prints = () => {
-    const products = [
+
+    const printsSizes = [
+        {
+            name: "A5",
+            specific_size: "27cm x 10cm"
+        },
+        {
+            name: "A4",
+            specific_size: "27cm x 20cm"
+        },
+        {
+            name: "A3",
+            specific_size: "27cm x 30cm"
+        }
+    ]
+
+    const printsProducts = [
         {
             id: 7,
             image: printOvelhaNegra,
             name: "Print Ovelha Negra",
             price: "8,50",
-            sizes: ["A5", "A4", "A3"]
+            description: "Impressão em papel couchê fosco com gramatura 300.",
+            sizes: printsSizes
         },
         {
             id: 8,
             image: printBesouro,
             name: "Print Besouro",
             price: "8,50",
-            sizes: ["A5", "A4", "A3"]
+            description: "Impressão em papel couchê fosco com gramatura 300.",
+            sizes: printsSizes
         },
         {
             id: 9,
             image: printFundoDoMar,
             name: "Print Fundo do Mar",
             price: "8,50",
-            sizes: ["A5", "A4", "A3"]
+            description: "Impressão em papel couchê fosco com gramatura 300.",
+            sizes: printsSizes
         },
         {
             id: 10,
             image: printEspelho,
             name: "Print Espelho",
             price: "8,50",
-            sizes: ["A5", "A4", "A3"]
+            description: "Impressão em papel couchê fosco com gramatura 300.",
+            sizes: printsSizes
         },
         {
             id: 11,
             image: printAbducao,
             name: "Print Abdução",
             price: "8,50",
-            sizes: ["A5", "A4", "A3"]
+            description: "Impressão em papel couchê fosco com gramatura 300.",
+            sizes: printsSizes
         }
     ];
 
@@ -52,7 +73,7 @@ const Prints = () => {
         <>
             <HighlightBanner imageSrc={bannerPrints} altText="prints" />
             <main className="bodyWrapper">
-                <Catalog name="Prints em Papel Couchê" type="printsCatalog" products={products}/>
+                <Catalog name="Prints em Papel Couchê" type="printsCatalog" products={printsProducts}/>
             </main>
         </>
     )
--- jagua_app/src/pages/Stickers.js
+++ jagua_app/src/pages/Stickers.js
@@ -11,48 +11,61 @@ import adesivoBesouroPreto from "../assets/visual_identity/adesivos/adesivo_beso
 import adesivoBesouro from "../assets/visual_identity/adesivos/adesivo_besouro.png";
 
 const Stickers = () => {
-    const products = [
+    const stickerSizes = [
+        {
+            name: "Tam. Único",
+            specific_size: "2cm x 3,5cm"
+        },
+
+    ]
+    const stickersProducts = [
         {
             id: 1,
             image: adesivoArcoIris,
             name: "Adesivo Arco-íris",
             price: "2,90",
-            sizes: ["P", "M", "G"]
+            description: "Adesivo de vinil à prova-d'água.",
+            sizes: stickerSizes
         },
         {
             id: 2,
             image: adesivoRa,
             name: "Adesivo Rã",
             price: "2,90",
-            sizes: ["P", "M", "G"]
+            description: "Adesivo de vinil à prova-d'água.",
+            sizes: stickerSizes
         },
         {
             id: 3,
             image: adesivoVasoAzul,
             name: "Adesivo Vaso Azul",
             price: "2,90",
-            sizes: ["P", "M", "G"]
+            description: "Adesivo de vinil à prova-d'água.",
+            sizes: stickerSizes
         },
         {
             id: 4,
             image: adesivoCoracao,
             name: "Adesivo Coração",
             price: "2,90",
-            sizes: ["P", "M", "G"]
+            description: "Adesivo de vinil à prova-d'água.",
+            sizes: stickerSizes
         },
         {
             id: 5,
             image: adesivoBesouroPreto,
             name: "Adesivo Besouro Preto",
             price: "2,90",
-            sizes: ["P", "M", "G"]
+            description: "Adesivo de vinil à prova-d'água.",
+            sizes: stickerSizes
         },
         {
             id: 6,
             image: adesivoBesouro,
             name: "Adesivo Besouro",
             price: "2,90",
-            sizes: ["P", "M", "G"]
+            description: "Adesivo de vinil à prova-d'água.",
+            sizes: stickerSizes
         }
     ];
 
@@ -60,7 +73,7 @@ const Stickers = () => {
         <>
             <HighlightBanner imageSrc={bannerAdesivos} altText="adesivos" />
             <main className="bodyWrapper">
-                <Catalog name="Adesivos de Vinil" type="stickersCatalog" products={products} />
+                <Catalog name="Adesivos de Vinil" type="stickersCatalog" products={stickersProducts} />
             </main>
         </>
     );
