e3bb17cd341d9bf36a343f6133804b682d78f593
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2023-06-10 18:40:11 -0300

    começando a estilizar o botão de sair

--- jagua_app/src/components/ExitButton/ExitButton.js
+++ jagua_app/src/components/ExitButton/ExitButton.js
@@ -1,33 +1,28 @@
 import { Link } from "react-router-dom";
-
 import { useContext } from "react";
 import { StatusContext } from "../../App.js";
+import "./ExitButton.css"; // Importando o arquivo CSS
 
-const ExitButton = ({callBack}) => {
-    const {
-        setStatus
-    } = useContext(StatusContext);
+const ExitButton = ({ callBack }) => {
+  const { setStatus } = useContext(StatusContext);
 
-    const handleOnClick = () => {
-        setStatus({
-            type: "loggedOut",
-            user: null,
-            currProduct: null,
-            cartList: [],
-            orders: []
-        });
+  const handleOnClick = () => {
+    setStatus({
+      type: "loggedOut",
+      user: null,
+      currProduct: null,
+      cartList: [],
+      orders: []
+    });
 
-        callBack();
-    };
+    callBack();
+  };
 
-    return (
-        <Link 
-            to="/"
-            onClick={handleOnClick}
-        >
-            <h1>x</h1>
-        </Link>
-    );
+  return (
+    <Link to="/" onClick={handleOnClick} className="exit-button"> {/* Aplicando a classe "exit-button" */}
+      <h1>Sair</h1>
+    </Link>
+  );
 };
 
 export default ExitButton;
