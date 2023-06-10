c1fefbee32864f459bcb6d9942f7066821a87850
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2023-06-10 10:18:48 -0300

    localstorage

--- jagua_app/src/App.js
+++ jagua_app/src/App.js
@@ -27,11 +27,10 @@ import adesivoArcoIris from "./assets/visual_identity/adesivos/adesivo_arco_iris
 import printFundoDoMar from "./assets/visual_identity/prints/print_concha.png";
 //-----------------------------------
 
-
 export const StatusContext = createContext();
 
 function App() {
-  const [productCartList, setProductCartList] = useState("");
+  const [productCartList, setProductCartList] = useState([]);
 
   //lista de listas de produtos: quando uma compra é finalizada, a productCartList
   //é adicionada na ordersList 
@@ -69,7 +68,7 @@ function App() {
     qtd: 1,
     size_name: "A5"
   }];
-  
+
   const [ordersList, setOrdersList] = useState([
     {
       id: 1,
@@ -84,31 +83,37 @@ function App() {
       productList: productOrderList2,
       status: "Postado para envio",
       code: "AbcdeXy023"
-    }]);
+    }
+  ]);
 
   //----------apenas p/ teste--------------------
 
-  const [status, setStatus] = useState({
-    type: "loggedOut",
-    user: null,
-    currProduct: null,
-    cartList: productCartList,
-    orders: ordersList
+  const [status, setStatus] = useState(() => {
+    const storedStatus = localStorage.getItem("status");
+    return storedStatus ? JSON.parse(storedStatus) : {
+      type: "loggedOut",
+      user: null,
+      currProduct: null,
+      cartList: productCartList,
+      orders: ordersList
+    };
   });
 
+  useEffect(() => {
+    localStorage.setItem("status", JSON.stringify(status));
+  }, [status]);
 
   //as funcoes de manipulacao tambem devem ser visiveis por todos os 
   //componentes (devem estar descritas no componente pai)
   const handleAddToCart = (productAdded) => {
     const newProductCartList = [...productCartList, {
-          id: productAdded.id,//ver depois
-          image: productAdded.image,
-          name: productAdded.name,
-          price: productAdded.price,
-          qtd: productAdded.qtd,
-          size_name: productAdded.size_name
-        },
-    ];
+      id: productAdded.id, //ver depois
+      image: productAdded.image,
+      name: productAdded.name,
+      price: productAdded.price,
+      qtd: productAdded.qtd,
+      size_name: productAdded.size_name
+    }];
 
     setProductCartList(newProductCartList);
     setStatus((prevStatus) => ({
@@ -117,7 +122,7 @@ function App() {
     }));
   };
 
-  const handleRemoveItemCart = (rem_product) =>{
+  const handleRemoveItemCart = (rem_product) => {
     //removendo o item do carrinho
     const updatedList = productCartList.filter((product) => product !== rem_product);
     setProductCartList(updatedList);
@@ -125,7 +130,7 @@ function App() {
       ...prevStatus,
       cartList: updatedList
     }));
-}
+  }
   //-----------------------------------
 
   useEffect(() => {
@@ -141,7 +146,7 @@ function App() {
     >
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navbar handleRemoveItemCart={handleRemoveItemCart}/>}>
+          <Route path="/" element={<Navbar handleRemoveItemCart={handleRemoveItemCart} />}>
             <Route index element={<Home />} />
             <Route path="aboutMe" element={<AboutMe />} />
             <Route path="prints" element={<Prints />} />
@@ -149,7 +154,7 @@ function App() {
             <Route path="shirts" element={<Shirts />} />
             <Route path="login" element={<Login />} />
             <Route path="signUp" element={<SignUp />} />
-            <Route path="productDetails" element={<ProductDetails handleAddToCart={handleAddToCart}/>} />
+            <Route path="productDetails" element={<ProductDetails handleAddToCart={handleAddToCart} />} />
             <Route path="createNewProduct" element={<CreateNewProduct />} />
             <Route path="checkout" element={<Checkout />} />
             <Route path="editProfile" element={<EditProfile />} />
